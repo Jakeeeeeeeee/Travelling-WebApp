6ba427de577f26e7b0cd193b2eeaff5bbb1f5051
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-08-09 23:14:26 +1000

    introducing the ant design

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from './App.module.css';
+import { Layout, Typography } from 'antd';
 
 function App() {
   return (
     <div className={styles.App}>
-      1qwe
+      <div>
+        <Layout.Header>
+          <Typography.Title level={3}>Let's Travel</Typography.Title>
+        </Layout.Header>
+      </div>
     </div>
   );
 }
