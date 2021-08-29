bcdfd19fb779929e80fb57b3733a44e48abc50af
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-08-30 02:57:15 +1000

    feat(router dom): introducing the router path to different pages

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import styles from './App.module.css';
 import { Footer, Header } from '../components';
-import { HomePage } from '../pages/HomePage';
+import { HomePage, SignUpPage, LogInPage, TravelDetailPage } from '../pages';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-      <HomePage />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/signUp' component={SignUpPage} />
+          <Route path='/logIn' component={LogInPage} />
+          <Route path='/travelDetail/:travelRouteID' component={TravelDetailPage} />
+          <Route render={() => <h1>404 not found</h1>} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
